a6c731f4e85b7ad9c077d728c24aed8219d908e8
Bruno Bonamin <bruno@bonamin.org>
2013-02-21 15:23:08 -0300

    Template improvements

--- panel/panel.js
+++ panel/panel.js
@@ -4,15 +4,6 @@ $(document).ready(function(){
   $('#highlight').click(function(){
     chrome.extension.sendMessage('initSelectorHighlight', function(response) {});
   })
-
-  chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      alert('message fro client, by panel!');
-      if(request.message == 'newObject'){
-        alert('sent new object message@');
-      }
-    }
-  );
 //   $('#new_object_map').submit(function(e){
 //     // e.preventDefault();
 //     // alert('haha!');
